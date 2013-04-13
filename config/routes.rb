01bd0d6b307dff22bfda390e4eabe272a97d3849
LiuCong <diyumoshushi@gmail.com>
2013-04-13 15:52:35 +0800

    add Connect file

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ SampleApp::Application.routes.draw do
 
   get "static_pages/about"
 
+ match "static_pages/connect"=>"static_pages#connect"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
