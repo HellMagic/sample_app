2ccaf883fab54ec55cbcbb98745c4f3071e6ac0d
LiuCong <diyumoshushi@gmail.com>
2013-04-27 18:23:33 +0800

    test the git

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@
 class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  attr_accessible :email, :name, :password, :password_confirmation
+  attr_accessible :email, :name, :password, :password_confirmation#abc
   has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   has_many :followed_users, through: :relationships, source: :followed
